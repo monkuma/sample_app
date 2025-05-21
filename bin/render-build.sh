f20a138b3d7d4f0f8fa4c157c61ae387082f3418
monkuma <77419221+monkuma@users.noreply.github.com>
2025-05-21 12:23:20 +0000

    remove db reset

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -5,8 +5,3 @@ bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
 bundle exec rails db:migrate
-
-# If you want to reset the database, uncomment the following lines
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rails db:migrate:reset
-# If you want to seed the database, uncomment the following line
-bundle exec rails db:seed
