22f7954533bbee0328c9de259a815b961f68fb83
monkuma <77419221+monkuma@users.noreply.github.com>
2025-05-24 06:34:57 +0000

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d-]+(\.[a-z\d-]+)*\.[a-z]+\z/i
@@ -19,16 +21,32 @@ class User < ApplicationRecord
     BCrypt::Password.create(string, cost: cost)
   end
 
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
   # 永続化セッションのためユーザーをＤＢに記憶
-  # def remenber
-  #   self.remember_token = User.new_token
-  #   update_attribute(:remember_digest, User.digest(remember_token))
-  #   remember_digest
-  # end
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+    remember_digest
+  end
+
+  # 渡されたトークンがダイジェストと一致すればtrueを返す
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # ユーザーのログイン情報を破棄
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 
   # セッションハイジャック防止のためセッショントークンを返す
   # この記憶ダイジェストを再利用しているのは利便性のため
-  # def session_token
-  #   remember_digest || remember
-  # end
+  def session_token
+    remember_digest || remember
+  end
 end
